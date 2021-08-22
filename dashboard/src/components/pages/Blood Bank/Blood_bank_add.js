c4bf36f51d1a3fcacf0140cc1316fde26ccf04d1
MELRIAN1910 <89056743+MELRIAN1910@users.noreply.github.com>
2021-08-22 18:21:34 +0530

    create page from melrian

---
+++ dashboard/src/components/pages/Blood Bank/Blood_bank_add.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Blood_bank_add() {
+    return (
+        <div>
+            This is Blood Bank Add page
+        </div>
+    )
+}
+
+export default Blood_bank_add
---
+++ dashboard/src/components/pages/Blood Bank/blood_bank_update.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function blood_bank_update() {
+    return (
+        <div>
+            This is Blood Bank Update Page
+        </div>
+    )
+}
+
+export default blood_bank_update
