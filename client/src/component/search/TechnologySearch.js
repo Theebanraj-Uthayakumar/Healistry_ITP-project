d7d5c704e7690c1ca7013a77b7b2f263024376c3
Theeban <69495468+Theebanraj-Uthayakumar@users.noreply.github.com>
2021-09-06 12:09:30 +0530

    issue fixed

--- client/src/component/search/TechnologySearch.js
+++ client/src/component/search/TechnologySearch.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { getTechnologies } from "../../api/Technology";
-import TextField from "@material-ui/core/TextField";
-import Autocomplete from "@material-ui/lab/Autocomplete";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
